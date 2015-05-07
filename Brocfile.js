b4a8ccc108f92aab8be23740fa8577c0679c6e07
Mark Stuart <mstuart_is@me.com>
2015-05-07 14:54:56 +1200

    Update Brocfile to use emberTemplateCompiler

--- Brocfile.js
+++ Brocfile.js
@@ -1,7 +1,14 @@
 var makeModules = require('broccoli-dist-es6-module');
 var templateFilter = require('ember-cli-htmlbars');
+var emberTemplateCompiler = require('./bower_components/ember/ember-template-compiler');
 
-var templates = templateFilter('lib', {module: true});
+var templates = templateFilter('lib',
+  {
+    module: true,
+    isHTMLBars: true,
+    templateCompiler: emberTemplateCompiler
+  }
+);
 module.exports = makeModules(templates, {
   global: 'ic.autocomplete',
   packageName: 'ic-autocomplete',
